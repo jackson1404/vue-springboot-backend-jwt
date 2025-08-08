7d65a770db6dfc5440cae905ca69acba005f61d7
jackson1404 <jackson14042004@gmail.com>
2025-08-08 22:18:13 +0630

    added method level security

---
+++ src/main/java/com/jackson/vue/jwt_backend_integrate/controller/AdminController.java
@@ -0,0 +1,23 @@
+package com.jackson.vue.jwt_backend_integrate.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    @GetMapping("/status")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> getStatus(){
+        return ResponseEntity.ok(Map.of("status", "ok"));
+    }
+
+}
