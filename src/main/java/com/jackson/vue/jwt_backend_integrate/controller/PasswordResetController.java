db72ce29c1c5da7d670b43e2024c7121ddb9de00
Win Min Thant <jackson14042004@gmail.com>
2025-08-13 13:14:43 +0630

    added forgot-password controller

---
+++ src/main/java/com/jackson/vue/jwt_backend_integrate/controller/PasswordResetController.java
@@ -0,0 +1,40 @@
+/***************************************************************
+ * Author       :	 
+ * Created Date :	
+ * Version      : 	
+ * History  :	
+ * *************************************************************/
+package com.jackson.vue.jwt_backend_integrate.controller;
+
+import com.jackson.vue.jwt_backend_integrate.service.PasswordResetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * PasswordResetController Class.
+ * <p>
+ * </p>
+ *
+ * @author
+ */
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class PasswordResetController {
+
+    private final PasswordResetService passwordResetService;
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<?> resetPassword(@RequestParam String email) throws Exception {
+        passwordResetService.createPasswordResetToken(email);
+        return ResponseEntity.ok("Password Reset Email Sent");
+    }
+
+    
+
+
+}
