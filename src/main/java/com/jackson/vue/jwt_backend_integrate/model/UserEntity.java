fa74bc8ee06ff850bea9a43a31ffea9f2bd5c367
Win Min Thant <jackson14042004@gmail.com>
2025-08-13 09:38:06 +0630

    added password reset token entity

--- src/main/java/com/jackson/vue/jwt_backend_integrate/model/UserEntity.java
+++ src/main/java/com/jackson/vue/jwt_backend_integrate/model/UserEntity.java
@@ -1,10 +1,9 @@
 package com.jackson.vue.jwt_backend_integrate.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tbl_employees")
@@ -21,5 +20,8 @@ public class UserEntity {
     private String username;
     private String password;
     private String role;
+
+    @OneToMany(mappedBy = "user")
+    private List<PasswordResetTokenEntity> passwordResetTokenEntities;
 
 }
